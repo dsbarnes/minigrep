e92afd1f22a6e614a84f5d69d87663098701be74
Derek Barnes <dsbarnes@Dereks-iMac.attlocal.net>
2020-08-23 19:58:51 -0400

    Section 12.1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Each branch of this repo represents a chapter from the Rust Book.");
-    println!("This is the master branch, and is empty, but for this.")
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for: {}", query);
+    println!("In file {} ", filename);
 }
