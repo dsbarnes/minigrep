eb9cd61395d283efc4bc10cccb1a84b39dcba6ee
Derek Barnes <dsbarnes@Dereks-iMac.attlocal.net>
2020-08-24 10:41:41 -0400

    new test passes

--- src/lib.rs
+++ src/lib.rs
@@ -26,15 +26,27 @@ impl Config {
     }
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
+    let mut result = Vec::new();
 
     for line in contents.lines() {
         if line.contains(query){
-            results.push(line);
+            result.push(line);
         }
     }
-    results
+    result
 }
 
 
@@ -43,13 +55,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result(){
+    fn case_sensative(){
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct Tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents));
     }
 }
